734a4714c7cc012278fc8f49e8f02f7b9a393c44
nayeven allaghen <nayevenallaghen17@gmail.com>
2022-10-15 12:12:39 +0400

    validation model car

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,9 @@
 class Car < ApplicationRecord
-  belongs_to :user
+  validates :brand, presence: true
+  validates :model, presence: true
+  validates :color, presence: true
+  validates :year, presence: true, numericality: true
+  validates :price, presence: true, numericality: true
+  # belongs_to :user
+
 end
