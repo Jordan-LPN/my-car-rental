92598a815499f9b0e4228e8b08c976808bd46f8c
jordan li piang nam <jordanelpn@gmail.com>
2022-10-20 20:56:36 +0400

    homepage front updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_15_113255) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_18_154018) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "cars", force: :cascade do |t|
     t.string "brand"
